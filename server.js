0637ee47f57766fc852e62609e7a86e70a5e2e4a
lIE200310 <kharismaliera@gmail.com>
2025-11-27 05:09:56 +0000

    BAGO

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to my page.");
-  res.end();
+  res.write(`
+    <h1 style = "font-family: Arial;"> Abeles, Liera V.</h1>
+    <h2>NT-4101</h2>
+    <p><i>"Just Give Up!</i><p>
+    `);
+    res.end();
 });
 
 server.listen(PORT, () => {
